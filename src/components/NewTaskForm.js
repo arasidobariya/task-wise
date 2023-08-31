137eceb792bcb16fbd22f8ef94a3e6a35d10be5c
Arasi Dobariya <arasidobariya7@gmail.com>
2023-08-31 09:37:11 -0400

    Created NewTaskForm

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,27 +1,46 @@
 import React from 'react';
-import { Card, TextField } from '@mui/material';
-
+import { Card, TextField, Grid, Stack, Button } from '@mui/material';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import styles from './NewTaskForm.module.css'
 function NewTaskForm() {
     return (
-        <div>
-            <Card>
-                <TextField
-                    id="standard-textarea"
-                    label="Task"
-                    placeholder="Add New Task"
-                    multiline
-                    variant="standard"
-                />
-                <TextField
-                    required
-                    id="standard-required"
-                    label="Owner"
-                    placeholder="Add Owner Name"
-                    variant="standard"
-                />
 
-            </Card>
-        </div>
+        <Card className={styles.formlayout} variant="outlined">
+            <Grid container spacing={3}>
+                <Grid item xs={4} md={6}>
+                    <TextField fullWidth required
+                        id="standard-required"
+                        label="Task"
+                        placeholder="Add New Task"
+                        multiline
+                        variant="standard"
+                    /> </Grid>
+                <Grid item xs={4}>
+                    <TextField fullWidth
+                        required
+                        id="standard-required"
+                        label="Owner"
+                        placeholder="Add Owner Name"
+                        variant="standard"
+                    /></Grid>
+                <Grid item xs={4}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs} className={styles.DatePicker}>
+                        <DemoContainer components={['DatePicker']}>
+                            <DatePicker label="Date of completion" />
+                        </DemoContainer>
+                    </LocalizationProvider>
+                </Grid>
+            </Grid>
+            <Stack spacing={2} direction="row" className={styles.stack}>
+                <Button variant="contained" className={styles.button}>Cancel</Button>
+                <Button variant="contained" color="success" className={styles.button}>Save</Button>
+
+            </Stack>
+        </Card>
+
     )
 }
 
